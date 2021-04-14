9036a3c4bc8eedd74954ee48f41003addcc5fce0
thomasohagan <thomasoh376@gmail.com>
2021-04-14 12:53:57 +0100

    new

--- property_prediction/data_utils.py
+++ property_prediction/data_utils.py
@@ -2,11 +2,10 @@
 """
 Module containing data loading utility functions.
 """
-import networkx as nx
-from pysmiles import read_smiles
+
 import numpy as np
 import pandas as pd
-from rdkit.Chem import AllChem, Descriptors, MolFromSmiles, rdmolops
+#from rdkit.Chem import AllChem, Descriptors, MolFromSmiles, rdmolops
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
