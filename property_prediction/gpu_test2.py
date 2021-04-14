4537527f286177fbbc3fa4d4dbbed1611a36ad93
thomasohagan <thomasoh376@gmail.com>
2021-04-14 21:08:45 +0100

    df

--- property_prediction/gpu_test2.py
+++ property_prediction/gpu_test2.py
@@ -4,5 +4,5 @@ with tf.device('/gpu:0'):
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
 
-with tf.compat.v1.Session() as sess:
-    print(sess.run(c))
+    with tf.compat.v1.Session() as sess:
+        print(sess.run(c))
