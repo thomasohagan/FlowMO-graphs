5f91d81a9c93634acc971c75835b6178d2d83639
thomasohagan <thomasoh376@gmail.com>
2021-05-08 20:34:04 +0100

    d

--- GP/kernels.py
+++ GP/kernels.py
@@ -115,10 +115,10 @@ class CWexp(gpflow.kernels.Kernel):
                     G2.append((read_smiles(h)))
 
         kernel_options = {'directed': False, 'compute_method': 'exp'}
-        graph_kernel = gklearn.kernels.CommonWalk(node_labels=[], edge_labels=[], **kernel_options,)
+        graph_kernel = gklearn.kernels.CommonWalk(node_labels=[], edge_labels=[], ds_infos={}, **kernel_options,)
         kernel = []
         for i in range(len(G2)):
-            kernel_list, run_time = graph_kernel.compute(G1, G2[i], parallel='imap_unordered', n_jobs=multiprocessing.cpu_count(), verbose=2, directed=False)
+            kernel_list, run_time = graph_kernel.compute(G1, G2[i], parallel='imap_unordered', n_jobs=multiprocessing.cpu_count(), verbose=2) ##or add ds_infos here
             print(kernel_list)
             kernel.append(kernel_list)
 
