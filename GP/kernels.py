2d121a752acd305c0ee991f8151e0e586defec4e
thomasohagan <thomasoh376@gmail.com>
2021-05-11 17:45:08 +0100

    e

--- GP/kernels.py
+++ GP/kernels.py
@@ -175,6 +175,7 @@ class MK(gpflow.kernels.Kernel):
             kernel.append(kernel_list)
 
         kernel = tf.convert_to_tensor(kernel, dtype=tf.float64)
+        kernel = tf.transpose(kernel)
 
         return self.variance * kernel
 
